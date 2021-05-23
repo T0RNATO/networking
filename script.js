ad9b0678d787b00449d39c1270704a032a2798e7
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2021-05-23 13:21:19 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ var images = document.getElementsByTagName('img');
 var i;
 var count = 0;
 for (i = 0; i < images.length; i++) {
-    images[i].addEventListener("click", function(){spin()});
+    images[i].addEventListener("click",
+      function() {
+        spin()
+      }
+    )
 }
 function spin() {
   count = count + 360;
