39f6ecdce97ea5460a0da1ac153221637557d91a
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2021-05-26 10:07:51 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-var images = document.getElementsByTagName('img');
-var i;
-var count = 0;
-for (i = 0; i < images.length; i++) {
+function go() {
+  var images = document.getElementsByTagName('img');
+  var i;
+  var count = 0;
+  for (i = 0; i < images.length; i++) {
     images[i].addEventListener("click",
       function() {
-        spin()
+        count = count + 360;
+        for (i = 0; i < images.length; i++) {
+          images[i].style.transform = "rotate(" + count + "deg)"
+        }
       }
     )
-}
-function spin() {
-  count = count + 360;
-  for (i = 0; i < images.length; i++) {
-    images[i].style.transform = "rotate(" + count + "deg)"
   }
 }
+window.onload = go();
